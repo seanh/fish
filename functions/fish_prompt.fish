91cf6ae15cdff31f39601943f94994e87fb43ffe
Sean Hammond <git@snhmnd.fastmail.com>
2018-05-01 21:14:57 +0100

    Limit git branch to 10 chars

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -28,6 +28,7 @@ function fish_prompt -d "Write out the prompt"
     set -g __fish_git_prompt_color_upstream green
     set -g __fish_git_prompt_showdirtystate yes  # Shows + and * for staged and unstaged changes.
     set -g __fish_git_prompt_showuntrackedfiles yes  # Shows % if there are untracked files.
+    set -g __fish_git_prompt_shorten_branch_len '10'
     __fish_git_prompt "%s "
     set_color normal
 

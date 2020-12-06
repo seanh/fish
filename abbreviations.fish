21e899fc0c937b75d17a76c0873d5ce3a797bc9b
Sean Hammond <git@snhmnd.fastmail.com>
2020-12-06 15:55:17 +0000

    Remove some abbreviations I don't use

--- abbreviations.fish
+++ abbreviations.fish
@@ -4,20 +4,9 @@ abbr gd git diff
 abbr ga git add
 abbr gc git commit
 abbr gco git checkout
-abbr v vagrant
-abbr encrypt pg --symmetric
-abbr decrypt gpg
-abbr mirror wget -mkp --random-wait
 abbr rsync rsync --archive --verbose --human-readable --itemize-changes --progress --compress --checksum
 abbr rsync_to_fat32 rsync --archive --no-perms --no-group --no-owner --modify-window 1 --verbose --human-readable --itemize-changes
 abbr untar tar -zxvf
-abbr htest tox -- --strict --verbose --showlocals
 abbr gvr gvim --remote-silent
 abbr xclip 'xclip -selection clipboard -i'
-abbr sc standup category
-abbr sn standup note
-abbr sp standup print
-abbr sl standup ls
-abbr shred shred -vu
-abbr gedit gedit --new-window
 abbr title set FISH_TITLE
